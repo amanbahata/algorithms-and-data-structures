66da9e70e811520173cf3d289c908b5d65263ed9
Aman Bahata <bahataaman@gmail.com>
2017-09-14 18:20:22 +0100

    Minor twicks

--- gcd.java
+++ gcd.java
@@ -1,17 +1,17 @@
 public class Gcd{
 
-
 // Static method that uses recursion to calculate the great common devisor of two integers
 	public static int gcd(int a, int b) throws IllegalArgumentException {
 		if (a == 0 || b == 0) {
 			throw new IllegalArgumentException();
 		}
 		int reminder;
-		if (a > b) {
-			reminder = a % b;
-		}else{
-			reminder = b % a;
+		if (a < b) {
+			int temp = a;
+			a = b;
+			b = temp;
 		}
+		reminder = a % b; 
 		if (reminder == 0) {
 			return b;
 		}
